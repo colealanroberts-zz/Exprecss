941190ad36c83b145643949b72ebf2394cc769e7
colealanroberts <cole.alan.roberts@gmail.com>
2014-05-28 18:11:25 -0500

    [Dist] Dropdown menu now working properly

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,18 +1,32 @@
 function responsiveNavOpen() {
-    $('.navbar ul').toggleClass('nav-close nav-open');
+    $('.btn-responsive-nav').on('click', function(e) {
+        $('.navbar ul').toggleClass('nav-close nav-open');
+        e.preventDefault();
+    });
 }
 
 function responsiveNavClose() {
-    $('.navbar ul').toggleClass('nav-close nav-open');
+    $('.btn-responsive-nav-close').on('click', function(e) {
+        $('.navbar ul').toggleClass('nav-close nav-open');
+        e.preventDefault();
+    });
+}
+
+function dropdownMenu() {
+    $('.dropdown-menu').addClass('dropdown-menu-inactive');
+    $('.btn-dropdown').click(function(e) {
+        $('.dropdown-menu').toggleClass('dropdown-menu-inactive dropdown-menu-active');
+        e.preventDefault();
+    });
 }
 
 $(document).ready(function() {
-    $('.btn-responsive-nav').on('click', function(e) {
-        responsiveNavOpen();
-        e.preventDefault();
-    });
-    $('.btn-responsive-nav-close').on('click', function(e) {
-        responsiveNavClose();
-        e.preventDefault();
-    });
+    'use strict';
+
+    // Dropdown
+    dropdownMenu();
+
+    // Responsive Nav
+    responsiveNavOpen();
+    responsiveNavClose();
 });
