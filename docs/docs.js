69264941b28c24883df673263ec1d780c5e2442c
colealanroberts <cole.alan.roberts@gmail.com>
2014-09-09 18:26:41 -0500

    [Docs] Update documentation and give it a bit of a facelift

--- docs/docs.js
+++ docs/docs.js
@@ -1,21 +1,3 @@
-var arr = ['mobile optimized', 'beautifully simple', 'scalable front-end', 'lightweight responsive'];
-var i = 0;
-var len = arr.length;
-var $el = $('#description-rotate');
-var $temp = $('<span />');
-var dev = true;
-
-function loop() {
-    $temp.hide().appendTo($el.parent());
-    var width = $temp.text(arr[i%=len]).width();
-    $el.fadeTo(1000, 0).animate({
-        width: width
-    }, 300, function() {
-        $(this).text(arr[i++]).fadeTo(1000, 1);
-    });
-    setTimeout(loop, 4000);
-}
-
 function nodeDepth() {
     if (dev == true) {
         var nKey = 78;
