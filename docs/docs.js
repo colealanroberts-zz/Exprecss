138f23d4d790db7e49c9efc73203bcf5a9f0b18a
colealanroberts <cole.alan.roberts@gmail.com>
2014-10-15 16:28:47 -0500

    [Docs] Updated docs for validation classes

--- docs/docs.js
+++ docs/docs.js
@@ -1,16 +1,6 @@
-function nodeDepth() {
-    if (dev == true) {
-        var nKey = 78;
-        $(document).keydown(function(e) {
-            if(e.which == nKey) {$('body').toggleClass('node-depth');
-            }
-        });
-    } else {
-        console.log('var dev is set to ' + dev);
-    }
-}
-
-$(document).ready(function() {
-    nodeDepth(dev);
+// Simple Validator Exmaple
+$(function() {
+    $('.btn-validate-me').click(function() {
+        $('.validation-text').addClass('animation-validate');
+    });
 });
-
